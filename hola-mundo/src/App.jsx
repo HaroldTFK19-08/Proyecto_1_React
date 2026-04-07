cc44cb0d30b8dcc1946868b5f5db57d4bd4ea2ad
HaroldTFK19-08 <haroldsalazar823@gmail.com>
2026-04-07 12:40:50 -0500

    correcciones

--- hola-mundo/src/App.jsx
+++ hola-mundo/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./App.css"
-//importa
+//importamos los componentes de nuestra pagina web
 import Inicio from "./pages/Inicio"
 import SeccionPrincipal from "./pages/seccionPrincipal";
 import SeccionSecundaria from "./pages/seccionSecundaria";
@@ -8,6 +8,7 @@ import Aside from "./pages/Aside"
 import Footer from './pages/Footer'
 const App = () => {
   return (
+    //dentro de un div esta la estructura de mi pagina web
     <div>
       <Inicio /> 
       <div className='div1'>
